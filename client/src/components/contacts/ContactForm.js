b7c500f9cd926d218fd8052bddf6eb498877fb8b
jstone18 <jason.stone0808@gmail.com>
2020-03-05 22:28:56 -0500

    Update contact form's edit functionality (fully working)

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,8 +1,23 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import ContactContext from "../../context/contact/contactContex";
 
 const ContactForm = () => {
 	const contactContext = useContext(ContactContext);
+
+	const { addContact, current, clearCurrent, updateContact } = contactContext;
+
+	useEffect(() => {
+		if (current !== null) {
+			setContact(current);
+		} else {
+			setContact({
+				name: "",
+				email: "",
+				phone: "",
+				type: "personal"
+			});
+		}
+	}, [contactContext, current]);
 
 	const [contact, setContact] = useState({
 		name: "",
@@ -20,19 +35,24 @@ const ContactForm = () => {
 	const onSubmit = e => {
 		e.preventDefault();
 
-		contactContext.addContact(contact);
+		if (current === null) {
+			addContact(contact);
+		} else {
+			updateContact(contact);
+		}
 
-		setContact({
-			name: "",
-			email: "",
-			phone: "",
-			type: "personal"
-		});
+		clearAll();
+	};
+
+	const clearAll = () => {
+		clearCurrent();
 	};
 
 	return (
 		<form onSubmit={onSubmit}>
-			<h2 className="text-primary">Add Contact</h2>
+			<h2 className="text-primary">
+				{current ? "Edit Contact" : "Add Contact"}
+			</h2>
 			<input
 				type="text"
 				placeholder="Name"
@@ -74,10 +94,17 @@ const ContactForm = () => {
 			<div>
 				<input
 					type="submit"
-					value="Add Contact"
+					value={current ? "Update Contact" : "Add Contact"}
 					className="btn btn-primary btn-block"
 				/>
 			</div>
+			{current && (
+				<div>
+					<button className="btn btn-light btn-block" onClick={clearAll}>
+						Clear
+					</button>
+				</div>
+			)}
 		</form>
 	);
 };
