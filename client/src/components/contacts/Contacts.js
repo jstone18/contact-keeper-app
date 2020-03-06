c06b27c181c1527aa8254eedff4bdf4d32fd1604
jstone18 <jason.stone0808@gmail.com>
2020-03-05 22:58:09 -0500

    Create contact filter (working)

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -5,12 +5,21 @@ import ContactItem from "./ContactItem";
 const Contacts = () => {
 	const contactContex = useContext(ContactContext);
 
-	const { contacts } = contactContex;
+	const { contacts, filtered } = contactContex;
+
+	if (contacts.length === 0) {
+		return <h4>You currently have no contacts listed.</h4>;
+	}
+
 	return (
 		<Fragment>
-			{contacts.map(contact => (
-				<ContactItem key={contact.id} contact={contact} />
-			))}
+			{filtered !== null
+				? filtered.map(contact => (
+						<ContactItem key={contact.id} contact={contact} />
+				  ))
+				: contacts.map(contact => (
+						<ContactItem key={contact.id} contact={contact} />
+				  ))}
 		</Fragment>
 	);
 };
