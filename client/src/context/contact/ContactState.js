2a41ba84f7803f18d0decb13519c4ef851b3c234
jstone18 <jason.stone0808@gmail.com>
2020-03-07 01:38:27 -0500

    Update contacts (working)

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -76,6 +76,28 @@ const ContactState = props => {
 		}
 	};
 
+	// Update Contact
+	const updateContact = async contact => {
+		const config = {
+			headers: {
+				"Content-Type": "application/json"
+			}
+		};
+
+		try {
+			const res = await axios.put(`/contacts/${contact._id}`, contact, config);
+			dispatch({
+				type: UPDATE_CONTACT,
+				payload: res.data
+			});
+		} catch (err) {
+			dispatch({
+				type: CONTACT_ERROR,
+				payload: err.response.data.msg
+			});
+		}
+	};
+
 	// Clear Contacts
 	const clearContacts = () => {
 		dispatch({ type: CLEAR_CONTACTS });
@@ -92,14 +114,6 @@ const ContactState = props => {
 	// Clear Current Contact
 	const clearCurrent = () => {
 		dispatch({ type: CLEAR_CURRENT });
-	};
-
-	// Update Contact
-	const updateContact = contact => {
-		dispatch({
-			type: UPDATE_CONTACT,
-			payload: contact
-		});
 	};
 
 	// Filter Contacts
--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -25,6 +25,14 @@ export default (state, action) => {
 				contacts: [action.payload, ...state.contacts],
 				loading: false
 			};
+		case UPDATE_CONTACT:
+			return {
+				...state,
+				contacts: state.contacts.map(contact =>
+					contact._id === action.payload._id ? action.payload : contact
+				),
+				loading: false
+			};
 		case DELETE_CONTACT:
 			return {
 				...state,
@@ -50,14 +58,6 @@ export default (state, action) => {
 			return {
 				...state,
 				current: null
-			};
-		case UPDATE_CONTACT:
-			return {
-				...state,
-				contacts: state.contacts.map(contact =>
-					contact.id === action.payload.id ? action.payload : contact
-				),
-				loading: false
 			};
 		case FILTER_CONTACTS:
 			return {
